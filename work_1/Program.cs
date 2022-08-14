6f96d9e6b813ffc5e88096ea548e295d4ce1172e
marina <litvinova_06@bk.ru>
2022-08-14 16:21:07 +0500

    work1

--- work_1/Program.cs
+++ work_1/Program.cs
@@ -4,29 +4,29 @@
 // 782 -> 8
 // 918 -> 1
 
-// int [] array = {4,5,6};
-// int n = array.Length;
-// int find = 5;
-// int index = 0;
-// while (index < n)
-// {
-//     {if (array [index]==find);
-//     }
-//     index++;
-// }
-// Console.WriteLine(array[1]);
+int [] array = {4,5,6};
+int n = array.Length;
+int find = 5;
+int index = 0;
+while (index < n)
+{
+    {if (array [index]==find);
+    }
+    index++;
+}
+Console.WriteLine(array[1]);
 
-// int [] array = {7,8,2};
-// int n = array.Length;
-// int find = 8;
-// int index = 0;
-// while (index < n)
-// {
-//     {if (array [index]==find);
-//     }
-//     index++;
-// }
-// Console.WriteLine(array[1]);
+int [] array = {7,8,2};
+int n = array.Length;
+int find = 8;
+int index = 0;
+while (index < n)
+{
+    {if (array [index]==find);
+    }
+    index++;
+}
+Console.WriteLine(array[1]);
 
 int [] array = {9,1,8};
 int n = array.Length;
